586ef5440d236a36ddb2937d86bbc994f709f479
Gino <neodracarys@gmail.com>
2020-04-27 14:22:43 +0800

    Commit for completed week 2

--- app/app.js
+++ app/app.js
@@ -2,18 +2,31 @@
 'use strict';
 
 angular.module('myFirstApp', [])
+.filter('customToLowerCase', LowercaseFilterFactory)
 .controller('DIController', DIController);
 
-DIController.$inject= ['$scope', '$filter']; //for minification proof
+DIController.$inject= ['$scope', '$filter', 'customToLowerCaseFilter']; //for minification proof
 
-function DIController($scope, $filter){
+function DIController($scope, $filter, customToLowerCaseFilter){
     $scope.name="Gino";
 
     $scope.upper= () => {
       let upCase= $filter('uppercase');
       $scope.name= upCase($scope.name)
     };
+
+    $scope.lower= () => {
+        $scope.name= customToLowerCaseFilter($scope.name);
+    }
 }
+
+function LowercaseFilterFactory(){
+    return function (input) {
+        input= input || "";
+        return input.toLowerCase();
+    }
+}
+
 })();
 
 
